c5accd04e8e3bc09ebed904ea63e83fc00d18d55
Ashley Williams <ashley@bocoup.com>
2015-04-25 19:13:01 -0400

    take out / typo in host name

--- public/js/api-browser.js
+++ public/js/api-browser.js
@@ -2,7 +2,7 @@ var input = document.getElementsByTagName('input')[0];
 
 var makeReq = function() {  
   
-  var host = "http://45.55.231.22/:8080";  
+  var host = "http://45.55.231.22:8080";  
 
   url = host + input.value;
 
