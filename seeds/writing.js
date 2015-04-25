5c856fb946229928187729eab99000e540ff94af
Ashley Williams <ashley@bocoup.com>
2015-04-25 13:15:14 -0400

    add first pass at seeds

---
+++ seeds/writing.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.seed = function(knex, Promise) {
+    return Promise.join(
+        // Deletes ALL existing entries
+        knex('writing').del(), 
+
+        // Inserts seed entries
+        knex('writing').insert({
+          id: 1,
+          title: 'No True Conference Organizers',
+          url: 'https://medium.com/@ag_dubs/no-true-conference-organizer-dd0ff11294a',
+          summary: 'a response to Jared Spool\'s post "Code of Conducts[sic]: Considered Harmful"'
+        })
+    );
+};
