64bda4e8c8df8a45c1799260b3fa347a4ea540f2
Ashley Williams <ashley@bocoup.com>
2015-04-25 17:50:18 -0400

    add API explorer

--- app/index.js
+++ app/index.js
@@ -12,10 +12,23 @@ const app = express();
 
 const API = require('./classes/api');
 
+const cors = require('express-cors');
+ 
+app.use(cors({
+    allowedOrigins: [
+        'localhost:8000', 'ashleygwilliams.github.io'
+    ]
+}));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({
   type: ['application/json', 'application/vnd.api+json']
 }));
+
+app.use(express.static('public'));
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
 resources.forEach(function (resource) {
   API.register(resource);
@@ -25,6 +38,14 @@ resources.forEach(function (resource) {
 app.get('/', function (request, response) {
   response.set('Content-Type', 'application/json');
   response.send(JSON.stringify(API.index(), null, 2));
+});
+
+app.get('/ashleygwilliams', function(request, response) {
+  response.redirect('/users/1?include=presentations,projects,writing,community');
+});
+
+app.get('/test', function(request, response) {
+  response.render('dynamic_index');
 });
 
 module.exports = app;
---
+++ public/js/api-browser.js
@@ -0,0 +1,29 @@
+var input = document.getElementsByTagName('input')[0];
+
+var makeReq = function() {  
+  
+  url = input.value;
+
+  var output = document.getElementsByTagName('textarea')[0];
+  function reqListener () {
+    output.innerHTML = this.responseText;
+  }
+
+  var request = new XMLHttpRequest();
+  request.onload = reqListener;
+
+  console.log(url);
+
+  request.open("get", url, true);
+  request.setRequestHeader('Content-Type', 'application/vnd.api+json');
+  request.setRequestHeader('Accept', 'application/vnd.api+json');
+  window.req= request;
+  request.send();
+};
+
+//Immediately request /ashleygwilliams
+(makeReq)();
+
+// On click, make a new request
+var button = document.getElementsByClassName('button')[0];
+button.addEventListener('click', makeReq);
