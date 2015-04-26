57d09959f66edde5f142b2554a45d7a304acf2cd
Ashley Williams <ashley@bocoup.com>
2015-04-26 11:59:37 -0400

    add simple URL validation

--- public/js/api-browser.js
+++ public/js/api-browser.js
@@ -1,4 +1,7 @@
+'use strict';
+
 var input = document.getElementsByTagName('input')[0];
+var output = document.getElementsByClassName('output')[0];
 
 var makeReq = function(e) {  
   
@@ -7,27 +10,33 @@ var makeReq = function(e) {
   }
 
   var host = "http://45.55.231.22:8080";  
+  
+  if (validate(input.value)) {
+    var url = host + input.value;
 
-  url = host + input.value;
+    function reqListener () {
+      var json_string = JSON.stringify(JSON.parse(this.responseText), null, 4);
+      output.innerHTML = json_string;
+    }
 
-  var output = document.getElementsByClassName('output')[0];
-  console.log(output);
-  function reqListener () {
-    var json_string = JSON.stringify(JSON.parse(this.responseText), null, 4);
-    window.res = this.responseText;
-    output.innerHTML = json_string;
+    var request = new XMLHttpRequest();
+    request.onload = reqListener;
+
+    request.open("get", url, true);
+    request.setRequestHeader('Content-Type', 'application/vnd.api+json');
+    request.setRequestHeader('Accept', 'application/vnd.api+json');
+    request.send();
+  } else {
+    output.innerHTML = "// Bad Request! \n Try `/`, it lists the available options!";
   }
+};
 
-  var request = new XMLHttpRequest();
-  request.onload = reqListener;
+var validate = function(path) {
+  var result;
 
-  console.log(url);
+  var regex = /^\/(?=users|community|projects|presentation|writing)/;
 
-  request.open("get", url, true);
-  request.setRequestHeader('Content-Type', 'application/vnd.api+json');
-  request.setRequestHeader('Accept', 'application/vnd.api+json');
-  window.req= request;
-  request.send();
+  return path.match(regex) || path === "/";
 };
 
 //Immediately request /ashleygwilliams
